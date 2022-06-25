7d0fe6e9c30eb05b50fb66ca73a1633d9e89a0c3
Nguyen Dinh Giang <dinhgiang2611@gmail.com>
2022-06-25 10:45:26 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,47 +1,72 @@
 import "antd/dist/antd.min.css";
 import React, { useState, useEffect } from "react";
-import { Table } from "antd";
+import { Table, Button } from "antd";
+import qs from "qs";
 import Columns from "./Columns";
 
 let dataSource: DataType[] = [];
 
 function App() {
-    const [data, setData] = useState([]);
-    const getData = () => {
-        fetch("data.json", {
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-        })
-            .then(function (res) {
-                return res.json();
-            })
-            .then(function (json) {
-                setData(json.t);
-                dataSource = json.t.issueList.map((item, i) => {
-                    i = i + 1;
-                    return {
-                        key: i,
-                        no: i++,
-                        turnNum: item.turnNum,
-                        openNum: item.openNum,
-                        openTime: new Date(item.openTime),
-                        detail: `${item.openTime}|${item.turnNum}|${item.detail}`,
-                    };
-                });
-            });
-    };
+    const [loading, setLoading] = useState(false);
+    const [nextPageSize, changePageSize] = useState(false);
+    const [pagination, setPagination] = useState({
+        page: 1,
+        pageSize: 10,
+        gameCode: "st45g",
+    });
+
     useEffect(() => {
-        getData();
-    }, []);
+        changePageSize(true);
+
+        if (nextPageSize) {
+            setLoading(true);
+            const getData = () => {
+                fetch(
+                    `https://sodo678.com/api/front/open/lottery/history/high/page?${qs.stringify(
+                        pagination
+                    )}`
+                )
+                    .then((res) => res.json())
+                    .then((json) => {
+                        setLoading(false);
+                        dataSource = json.rows?.map((item, i) => {
+                            i = i + 1;
+                            return {
+                                key: i,
+                                no: i++,
+                                turnNum: item.turnNum,
+                                openNum: item.openNum,
+                                openTime: new Date(item.openTime),
+                                detail: `${item.openTime}|${item.turnNum}|${item.detail}`,
+                            };
+                        });
+                    });
+            };
+
+            getData({ pagination });
+        }
+    }, [pagination, nextPageSize]);
+
+    const onChangePageSize = (size, event) => {
+        setPagination({
+            ...pagination,
+            pageSize: size,
+        });
+    };
+
     return (
         <div className="App">
-            <h1>{data.name} | Lịch sử</h1>
+            <h1>Siêu Tốc 45 Giây | Lịch sử</h1>
+
+            <Button onClick={(e) => onChangePageSize(10, e)}>10 Lượt Xổ</Button>
+            <Button onClick={(e) => onChangePageSize(30, e)}>30 Lượt Xổ</Button>
+            <Button onClick={(e) => onChangePageSize(50, e)}>50 Lượt Xổ</Button>
+
             <Table
                 dataSource={dataSource}
                 columns={Columns}
-                pagination={{ defaultPageSize: 10 }}
+                pagination={false}
+                loading={loading}
             />
         </div>
     );
